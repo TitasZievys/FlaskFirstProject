177822c11ead7649d128b12b39245cc12bf092e0
TitasZievys <zievys.titas@gmail.com>
2021-05-01 18:38:23 +0300

    Bootstrap and layout implementation

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
 from flask import Flask, redirect, url_for, render_template
-
 app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/home")
 def home():
     return render_template("index.html", content=["tim", "jason", "bill"])
+
+
+@app.route("/valves")
+def valves():
+    return render_template('valves.html', title='valves')
 
 
 if __name__ == "__main__":
