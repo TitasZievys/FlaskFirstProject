61f2d62577adf981bf3cfa1659cfe6de4c90d417
TitasZievys <zievys.titas@gmail.com>
2021-05-01 20:56:16 +0300

    All pages on main screen added and then cv pasted the code from navigation bar and main (part 2 tutorial))

--- run.py
+++ run.py
@@ -13,5 +13,25 @@ def valves():
     return render_template('valves.html', title='valves')
 
 
+@app.route("/automation")
+def automation():
+    return render_template('automation.html', title='automation')
+
+
+@app.route("/notifications")
+def notifications():
+    return render_template('notifications.html', title='automation')
+
+
+@app.route("/system")
+def system_activation():
+    return render_template('system_activation.html', title='automation')
+
+
+@app.route("/data")
+def data():
+    return render_template('data.html', title='automation')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
